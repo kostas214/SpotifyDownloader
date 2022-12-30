7a8912f41ba4500a4d3f2d14faf37aaf85f69647
kostas214pro <kdoukas2006@gmail.com>
2022-12-30 16:20:08 +0200

    Runtime errors catching
    In this commit i fixed alot of the runtime errors that occured in my app like the link being invalid or the internal server is down or the device is not connected to the internet and lastly the permission to access storage isnt given

--- app/src/main/java/com/example/SpotifyDownloader/MainActivity.kt
+++ app/src/main/java/com/example/SpotifyDownloader/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.SpotifyDownloader
 
 import android.Manifest
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
@@ -23,8 +27,7 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    @RequiresApi(Build.VERSION_CODES.M)
+    private  var SuccessCode : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         val py = Python.getInstance()
         val module = py.getModule("main")
         var songNames = listOf<PyObject>()
+        var code: Int? = null
+
+
+
+        println("songs : ${songNames}")
 
         binding.perms.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
@@ -52,11 +60,18 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
+
+
+
         }
 
         binding.download2.setOnClickListener {
-            binding.download2.isEnabled = false
-            binding.progressBar.progress = 0
+            SuccessCode = 0
+            code = 0
+
+
+            var deviceInternetAccess = isDeviceOnline(this.applicationContext)
+
 
             val saveDirectory =
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
@@ -66,8 +81,11 @@ class MainActivity : AppCompatActivity() {
                     "songSearchSpotify",
                     binding.PlaylistLinkEditText.text.toString()
                 ).asList().toList()
-                songNames = data
-                println(songNames)
+
+                songNames = data[0].asList()
+                SuccessCode = data[1].toInt()
+                println("Succes code $SuccessCode")
+
 
                 binding.progressBar.max = songNames.size
                 println("got data")
@@ -76,15 +94,77 @@ class MainActivity : AppCompatActivity() {
             val task2 = Thread {
 
                 var isCompleted = false
+                var ToastMessage : String
+                runOnUiThread {
+                    binding.download2.isEnabled = false
+                    binding.progressBar.progress = 0
+                    binding.PlaylistLinkEditText.isEnabled=false
+                    binding.perms.isEnabled=false
+                }
                 while (!isCompleted) {
+
                     if (binding.progressBar.progress == binding.progressBar.max) {
+
+                        if (SuccessCode == 2 )
+                        {
+                            ToastMessage = "Invalid Spotify Playlist Share Link"
+                        }
+                        else if (SuccessCode == 1){
+                             ToastMessage = "No internet Connection"
+                        }
+                        else {
+                            ToastMessage = "Other error"
+                        }
                         runOnUiThread {
                             binding.download2.isEnabled = true
-                            Toast.makeText(this, "Finished", Toast.LENGTH_SHORT).show()
+                            binding.PlaylistLinkEditText.isEnabled=true
+                            binding.perms.isEnabled=true
+                            Toast.makeText(this,ToastMessage , Toast.LENGTH_SHORT).show()
                             println("End")
                         }
                         isCompleted = true
 
+                    }
+                    if(code == 1){
+                        runOnUiThread {
+                            Toast.makeText(this,"Connection Error",Toast.LENGTH_SHORT).show()
+                            binding.download2.isEnabled = true
+                            binding.PlaylistLinkEditText.isEnabled=true
+                            binding.perms.isEnabled=true
+                            binding.progressBar.progress = 0
+
+
+
+                        }
+                        isCompleted = true
+
+
+                    }
+                    else if (code == 2){
+                        runOnUiThread {
+                            Toast.makeText(this,"Internal Server is not working",Toast.LENGTH_SHORT).show()
+                            binding.download2.isEnabled = true
+                            binding.PlaylistLinkEditText.isEnabled=true
+                            binding.perms.isEnabled=true
+                            binding.progressBar.progress = 0
+
+
+                        }
+                        isCompleted = true
+
+                    }
+                    else if (code == 3){
+                        runOnUiThread {
+                            Toast.makeText(this,"Permission not given",Toast.LENGTH_SHORT).show()
+                            binding.download2.isEnabled = true
+                            binding.PlaylistLinkEditText.isEnabled=true
+                            binding.perms.isEnabled=true
+                            binding.progressBar.progress = 0
+
+
+
+                        }
+                        isCompleted = true
                     }
                 }
             }
@@ -93,18 +173,28 @@ class MainActivity : AppCompatActivity() {
 
 
             GlobalScope.launch {
+                println(SuccessCode)
                 var run = true
+
                 while (run) {
                     val isRunning = task1.isAlive
                     if (!isRunning) {
                         val executor = Executors.newFixedThreadPool(4)
                         for (i in songNames) {
                             val worker = Runnable {
-                                module.callAttr("DownloadSongs", i, saveDirectory)
-                                binding.progressBar.incrementProgressBy(1)
+                                 code = module.callAttr("DownloadSongs", i, saveDirectory).toInt()
+                                 binding.progressBar.incrementProgressBy(1)
+                                if (code == 1 || code == 2 || code == 3){
+                                    executor.shutdownNow()
+
+
+                                }
+
                             }
 
-                            executor.execute(worker)
+                            if (SuccessCode == 0 && deviceInternetAccess) {
+                                executor.execute(worker)
+                            }
                             run = false
                         }
                     }
@@ -129,4 +219,28 @@ private fun createDirectory(subfoldername: String): String {
 
 }
 
-
+fun isDeviceOnline(context: Context ): Boolean {
+    val connManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities =  connManager.getNetworkCapabilities(connManager.activeNetwork)
+        if (networkCapabilities == null) {
+            println( "Device Offline")
+            return false
+        }
+        else {
+            println( "Device Online")
+            return true
+        }
+    } else {
+        // below Marshmallow
+        val activeNetwork = connManager.activeNetworkInfo
+        if (activeNetwork?.isConnectedOrConnecting == true && activeNetwork.isAvailable) {
+            println( "Device Online")
+            return true
+        }
+        else {
+            println( "Device Offline")
+            return false
+        }
+    }
+}
--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,93 +1,117 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import string
+import httpx
 import music_tag
 import requests
-import SpotifyApiCredentials
+import spotipy
+import string
 import time
+from httpx import ReadError
+from requests.exceptions import ChunkedEncodingError
+from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython import VideosSearch
 
+import SpotifyApiCredentials
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SpotifyApiCredentials.SPOTIPY_CLIENT_ID,
-                                                                         client_secret=SpotifyApiCredentials.SPOTIPY_CLIENT_SECRET,
-                                                                         ))
-
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+    client_id=SpotifyApiCredentials.SPOTIPY_CLIENT_ID,
+    client_secret=SpotifyApiCredentials.SPOTIPY_CLIENT_SECRET,
+    ))
+print("hi")
 Failed = []
+
+
 def songSearchSpotify(playlistLink):
-    #Initialize Spotipy
-    #Store the songs of the playlist in a list
+    # Initialize Spotipy
+    # Store the songs of the playlist in a list
+    success = 0
     done = True
     offset = 0
     songs = []
-    tracks = sp.playlist_items(playlist_id=playlistLink, offset=offset)
-    for key in tracks['items']:
-        songs.append(f"{key['track']['name']} {key['track']['artists'][0]['name']}")
-    while done:
-        if len(songs) == offset + 100:
-            tracks = sp.playlist_items(playlist_id=playlistLink, offset=offset)
-            for key in tracks['items']:
-                songs.append(f"{key['track']['name']} {key['track']['artists'][0]['name']}")
-            offset += 100
-        if len(songs) < offset + 100:
-            done = False
+    try:
+        tracks = sp.playlist_items(playlist_id=playlistLink, offset=offset)
+    except requests.exceptions.ConnectionError:
+        success = 1
+        print("unable to connect to the internet")
+    except spotipy.exceptions.SpotifyException:
+        success = 2
+        print("Invalid Link")
 
-    return songs
+    if success == 0:
+        for key in tracks['items']:
+            songs.append(f"{key['track']['name']} {key['track']['artists'][0]['name']}")
+        while done:
+            if len(songs) == offset + 100:
+                try:
+                    tracks = sp.playlist_items(playlist_id=playlistLink, offset=offset)
+                except requests.exceptions.ConnectionError:
+                    success = False
+                    print("unable to connect to the internet")
+                for key in tracks['items']:
+                    songs.append(f"{key['track']['name']} {key['track']['artists'][0]['name']}")
+                offset += 100
+            if len(songs) < offset + 100:
+                done = False
+
+        return songs, success
+
+    else:
+        print("unable to get songs")
+        return songs, success
 
 
-
-
-
-def DownloadSongs(songs,filePath):
+def DownloadSongs(songs, filePath):
     urlTemplateForServerLocalHost = "http://192.168.2.17:5000/?link="
 
-    songSearch = VideosSearch(songs, limit = 1).result()
-    songId = songSearch['result'][0]['id']
-    songTitle = songSearch['result'][0]['title']
+    try:
+        st = time.time()
+        songSearch = VideosSearch(songs, limit=1).result()
+        tracks = sp.search(songs)
+        ArtWorkURL = tracks['tracks']['items'][0]['album']['images'][0]['url']
 
+        songId = songSearch['result'][0]['id']
+        songTitle = songSearch['result'][0]['title']
 
-    translation_table = str.maketrans('', '', string.punctuation)
-    safeString = songTitle.translate(translation_table)
-    fileName = safeString.replace(" ", "") + ".aac"
+        urlForServer = urlTemplateForServerLocalHost + songId
+        songData = requests.get(url=urlForServer)
+        response = requests.get(ArtWorkURL)
 
+        et = time.time()
+        elapsed_time = et - st
+        print(f'Execution time: {elapsed_time} seconds')
+        print(f"Downloads Failed {Failed}, Lenght of array  {len(Failed)}")
 
-    tracks = sp.search(songs)
+        translation_table = str.maketrans('', '', string.punctuation)
+        safeString = songTitle.translate(translation_table)
+        fileName = safeString.replace(" ", "") + ".aac"
 
+        albumName = tracks['tracks']['items'][0]['album']['name']
+        albumArtistName = tracks['tracks']['items'][0]['album']['artists'][0]['name']
+        albumTrackCount = tracks['tracks']['items'][0]['album']['total_tracks']
+        albumTrackNumber = tracks['tracks']['items'][0]['track_number']
+        releaseDate = tracks['tracks']['items'][0]['album']['release_date'][0:4]
+        artistName = tracks['tracks']['items'][0]['artists'][0]['name']
+        trackName = tracks['tracks']['items'][0]['name']
 
-    ArtWorkURL = tracks['tracks']['items'][0]['album']['images'][0]['url']
-    albumName = tracks['tracks']['items'][0]['album']['name']
-    albumArtistName = tracks['tracks']['items'][0]['album']['artists'][0]['name']
-    albumTrackCount = tracks['tracks']['items'][0]['album']['total_tracks']
-    albumTrackNumber = tracks['tracks']['items'][0]['track_number']
-    releaseDate = tracks['tracks']['items'][0]['album']['release_date'][0:4]
-    artistName = tracks['tracks']['items'][0]['artists'][0]['name']
-    trackName = tracks['tracks']['items'][0]['name']
+        fileLocation = fr"{filePath}/{fileName}"
+        open(fileLocation, 'wb').write(songData.content)
 
-
-
-
-    st = time.time()
-
-    urlForServer = urlTemplateForServerLocalHost + songId
-    fileLocation = fr"{filePath}/{fileName}"
-    songData = requests.get(url = urlForServer)
-    open(fileLocation,'wb').write(songData.content)
-
-    et = time.time()
-    elapsed_time = et - st
-    if elapsed_time>30:
-        Failed.append(songs)
-    print(f'Execution time: {elapsed_time} seconds')
-    print(f"Downloads Failed {Failed}, Lenght of array  {len(Failed)}")
-
-    response = requests.get(ArtWorkURL)
-    f = music_tag.load_file(fileLocation)
-    f['album'] = albumName
-    f['albumartist'] = albumArtistName
-    f['artist'] = artistName
-    f['artwork'] = response.content
-    f['totaltracks'] = albumTrackCount
-    f['tracknumber'] = albumTrackNumber
-    f['tracktitle'] = trackName
-    f['year'] = releaseDate
-    f.save()
-
+        f = music_tag.load_file(fileLocation)
+        f['album'] = albumName
+        f['albumartist'] = albumArtistName
+        f['artist'] = artistName
+        f['artwork'] = response.content
+        f['totaltracks'] = albumTrackCount
+        f['tracknumber'] = albumTrackNumber
+        f['tracktitle'] = trackName
+        f['year'] = releaseDate
+        f.save()
+        print("finished")
+        return 0
+    except (httpx.ConnectError, ChunkedEncodingError, ReadError):
+        print("ConnectionError")
+        return 1
+    except requests.exceptions.ConnectionError:
+        print("Internal server is down ")
+        return 2
+    except (PermissionError):
+        print("No permissions")
+        return 3
